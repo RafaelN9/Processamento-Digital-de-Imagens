04a6f58491263795a102147386e6a9d2305f3a6d
RafaelN9 <73660960+RafaelN9@users.noreply.github.com>
2021-07-26 09:29:31 -0300

    New Functions

--- first-step/read.py
+++ first-step/read.py
@@ -1,12 +1,13 @@
-#import matplotlib as mpl
-
-
 from tkinter import *
-from tkinter import filedialog 
-from cv2 import cvtColor, imread, COLOR_BGR2RGB
-import numpy as np
+from tkinter import filedialog
 
+import numpy as np
+import random as rng
+from cv2 import COLOR_BGR2RGB, cvtColor, imread
+from numpy.core.fromnumeric import shape
+from numpy.core.records import array
 from PIL import Image, ImageTk
+
 
 def to_hex(c):
     c = c[:3]
@@ -18,6 +19,8 @@ img = []
 newImg = []
 
 #FUNCTIONS OPERATIONS
+
+#F 1 ------------------------------------------------
 
 def grayScale():
     global img, newImg, panelsTop
@@ -140,8 +143,128 @@ def insertCanvas():
     panelsTop[0].bind( "<B1-Motion>", paint)    
     panelsTop[0].bind( "<Motion>", drawPos)  
 
+def noiseImages():
+    global img, newImg, panelsTop
+    if type(panelsTop[0]) is not Label:
+        return
+    if(len(img) > 0):
+        clearOtherPanels()
+        noisedImages = []
+        sumMatrix = np.zeros_like(img)
+        width = img.shape[0]-1
+        height = img.shape[1]-1
+        for i in range(10):
+            noisedImg = img
+            rngListW = rng.sample(range(width), 20)
+            rngListH = rng.sample(range(height), 20)
 
-    
+            for j in range(round(len(img)/10)):
+                noisedImg[rngListW[i], rngListH[i]] = 255
+                noisedImg[rngListW[i+1], rngListH[i+1]] = 0
+            sumMatrix[:,:] += noisedImg[:,:]
+            noisedImages.append(noisedImg)
+        sumMatrix[:,:] = np.around(sumMatrix[:,:]/10)
+        noisedImages.insert(0, sumMatrix)
+
+        def printSelImg(imgSel):
+            clearTopPanels()
+            imgSel = noisedImages[int(imgNamesList.get()[-1])]
+            createPanelTop(imgSel)
+
+        imgNames = []
+        for i in range(len(noisedImages)):
+            imgNames.append("Image "+str(i+1))
+
+        imgNamesList = StringVar()
+        imgNamesList.set(imgNames[0])
+        dropdown = OptionMenu(frameBottom, imgNamesList, *imgNames, command=printSelImg)
+        dropdown.pack(expand=True)
+        panelsBottom.append(dropdown)
+
+#F 2 ------------------------------------------------ 
+
+def dynamicCompression():
+    global img
+    if len(img) > 0:
+        clearOtherPanels()
+
+        def filterImg():
+            clearTopPanels()
+            constVar = float(const.get().replace(',',''))
+            gamaVar = float(gama.get().replace(',',''))
+            newImg = np.zeros_like(img)
+            for i in range(3):
+                newImg[:,:,i] = pow((constVar * img[:,:, i]), gamaVar)
+            createPanelTop(newImg)
+
+        frameToolBox = Frame(frameBottom)
+        frameToolBox.pack(pady=15)
+
+        labelConst = Label(frameToolBox, text="Insira a Constante")
+        labelConst.grid(row=0, column=1)
+        panelsBottom.append(labelConst)
+        const = StringVar()
+        inputConst = Entry(frameToolBox, width=20, textvariable=const)
+        const.set("1")
+        inputConst.grid(row=1, column=1)
+        panelsBottom.append(inputConst)
+
+        labelGama = Label(frameToolBox, text="Insira o Gama")
+        labelGama.grid(row=0, column=2)
+        panelsBottom.append(labelGama)
+        gama = StringVar()
+        inputGama = Entry(frameToolBox, width=20, textvariable=gama)
+        gama.set("1")
+        inputGama.grid(row=1, column=2)
+        panelsBottom.append(inputGama)
+        
+        btnFilter = Button(frameToolBox, command=filterImg, text="Fazer Compressão")
+        btnFilter.grid(row=0, rowspan=2, column=3, padx=10, sticky=NSEW)
+        panelsBottom.append(btnFilter)
+
+def average3x3():
+    global img, newImg
+    if len(img) > 0:
+        clearOtherPanels()
+        newImg = np.zeros_like(img)
+        for i in range(1,len(newImg)-1):
+            for j in range(1,len(newImg[0])-1):
+                                    #Left           Top Left        Top         Right        TopRight      Bottom       Bottom Left    Bottom Right
+                avgArray = np.array([img[i-1, j] ,img[i-1, j-1] ,img[i, j-1] ,img[i+1, j] ,img[i+1, j-1] ,img[i, j+1] ,img[i-1, j+1], img[i+1, j+1]])
+                newImg[i, j] = np.around(np.average(avgArray, axis=0)) 
+        createPanelTop(newImg)
+
+def median3x3():
+    global img, newImg
+    if len(img) > 0:
+        clearOtherPanels()
+        newImg = np.zeros_like(img)
+        for i in range(1,len(newImg)-1):
+            for j in range(1,len(newImg[0])-1):
+                                    #Left           Top Left        Top         Right        TopRight      Bottom       Bottom Left    Bottom Right
+                medianArray = np.array([img[i-1, j] ,img[i-1, j-1] ,img[i, j-1] ,img[i+1, j] ,img[i+1, j-1] ,img[i, j+1] ,img[i-1, j+1], img[i+1, j+1]])
+                medianArray = np.sort(medianArray, axis=0)
+                medianElement = medianArray[round(len(medianArray)/2)]
+                newImg[i, j] = medianElement
+        createPanelTop(newImg)
+
+def sobel():
+    global img, newImg
+    if len(img) > 0:
+        clearOtherPanels()
+        newImg = np.zeros_like(img)
+        imgGrayScale = np.zeros_like(img)
+        imgGrayScale = np.average(img, axis=2)
+        for i in range(1,len(newImg)-1):
+            for j in range(1,len(newImg[0])-1):
+                                            #Left           Top Left        Top             Right            TopRight      Bottom       Bottom Left    Bottom Right
+                sobelArrayX = np.array([imgGrayScale[i-1, j]*-2 ,imgGrayScale[i-1, j-1]*-1 ,0     ,imgGrayScale[i+1, j]*2 ,imgGrayScale[i+1, j-1]*1  ,0  ,imgGrayScale[i-1, j+1]*-1, imgGrayScale[i+1, j+1]*1])
+                sobelArrayY = np.array([ 0    ,imgGrayScale[i-1, j-1]*-1 ,imgGrayScale[i, j-1]*-2 ,0    ,imgGrayScale[i+1, j-1]*-1 ,imgGrayScale[i, j+1] ,imgGrayScale[i-1, j+1]*1, imgGrayScale[i+1, j+1]*1])
+                sobelElementX = np.sum(sobelArrayX)
+                sobelElementY = np.sum(sobelArrayY)
+                newImg[i, j] = np.around(np.sqrt(sobelElementX**2 + sobelElementY**2))
+        createPanelTop(newImg)
+
 #DEFAULT OPERATIONS
 
 def clearAll():
@@ -167,6 +290,7 @@ def select_image():
         else: 
             panelsTop[0].configure(image=imgPrint)
             panelsTop[0].image = imgPrint
+        noiseImages()
 
 def clearMainPanel():
     global panelsTop, panelsBottom
@@ -176,26 +300,35 @@ def clearMainPanel():
             panelsTop[0] = None
 
 def clearOtherPanels():
-    global panelsTop, panelsBottom, img
+    global panelsTop, img
     
-    if len(panelsTop) > 1:
-        for widget in frameTop.winfo_children()[1:]:
-            widget.destroy()
-        panelsTop = [frameTop.winfo_children()[0]]
-    if len(panelsBottom) > 0:
-        for widget in frameBottom.winfo_children():
-            widget.destroy()
-        panelsBottom = []
+    clearTopPanels()
+    clearBottomPanels()
+    
     if type(panelsTop[0]) is Canvas: 
         clearMainPanel()
         imgPrint = ImageTk.PhotoImage(Image.fromarray(cvtColor(img, COLOR_BGR2RGB)))
         panelsTop[0]= Label(frameTop, image=imgPrint)
         panelsTop[0].image = imgPrint
         panelsTop[0].pack(side="left", padx=10, pady=10)
-           
+
+def clearTopPanels():
+    global panelsTop
+    if len(panelsTop) > 1:
+        for widget in frameTop.winfo_children()[1:]:
+            widget.destroy()
+        panelsTop = [frameTop.winfo_children()[0]]
+
+def clearBottomPanels():
+    global panelsBottom
+    if len(panelsBottom) > 0:
+        for widget in frameBottom.winfo_children():
+            widget.destroy()
+        panelsBottom = []
+
 def setMainImg():
     global newImg, img, panelsTop
-    if len(newImg) > 0 and len(panelsTop > 1):
+    if len(newImg) > 0 and len(panelsTop) > 1:
         clearOtherPanels()
         img = newImg
         imgPrint = cvtColor(img, COLOR_BGR2RGB)
@@ -233,21 +366,6 @@ def createPanelBottom(image):
     newPanel.pack(side="left", padx=10, pady=10)
 
     panelsBottom.append(newPanel)
-    
-def printToPanelB(printImg):
-    global panelsTop, newImg
-    if len(printImg) > 0:
-        newImg = printImg
-        newImgPrint = cvtColor(newImg, COLOR_BGR2RGB)
-        newImgPrint = ImageTk.PhotoImage(Image.fromarray(newImgPrint)) 
-        if panelsTop[1] is None:
-            panelsTop[1] = Label(frameTop, image=newImgPrint)
-            panelsTop[1].image = newImgPrint
-            panelsTop[1].pack(side="right", padx=10, pady=10)
-        else:
-            panelsTop[1].configure(image=newImgPrint)
-            panelsTop[1].image = newImgPrint
-
 
 #--
 
@@ -277,7 +395,30 @@ functions.add_command(label="To Gray Scale", command=grayScale)
 functions.add_command(label="Invert Colors", command=invertColors)
 functions.add_command(label="Split Colors", command=splitRGB)
 functions.add_command(label="Insert Canvas Around Image", command=insertCanvas)
-menubar.add_cascade(label="Functions", menu=functions)
+menubar.add_cascade(label="Functions 1", menu=functions)
+functions2 = Menu(menubar, tearoff=0)
+functions2.add_command(label="Compressão de escala dinâmica", command=dynamicCompression)
+functions2.add_command(label="Limiarização", command=())
+functions2.add_command(label="Equalização de Histograma", command=())
+filters = Menu(functions2, tearoff=0)
+filters.add_command(label="Média 3x3", command=average3x3)
+filters.add_command(label="Mediana 3x3", command=median3x3)
+filters.add_command(label="Laplaciano", command=())
+filters.add_command(label="Sobel", command=sobel)
+functions2.add_cascade(label="Filters", menu=filters)
+menubar.add_cascade(label="Functions 2", menu=functions2)
+functions3 = Menu(menubar, tearoff=0)
+functions3.add_command(label="Mostrar transformada do cosseno", command=())
+functions3.add_command(label="Mostrar inversa da transformada", command=())
+functions3.add_command(label="Inserir ruído", command=())
+filters = Menu(functions2, tearoff=0)
+filters.add_command(label="Passa-alta", command=())
+filters.add_command(label="Passa-baixa", command=())
+functions3.add_cascade(label="Filtro na Transformada", menu=filters)
+filters = Menu(functions3, tearoff=0)
+menubar.add_cascade(label="Functions 3", menu=functions2)
+
+
 
 frameTop = Frame()
 frameTop.pack(side="top")
